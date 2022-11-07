ba0fb1842a8fcdbe62fb8321eaf2ed6b3228d7e2
SAMUEL ADEBAYO OLUWASEYI <adebayosamueljahsmine30@gmail.com>
2022-11-07 08:43:13 +0100

    included .env files

--- src/main/java/com/seyijs/springbootmongodb/SpringbootMongodbApplication.java
+++ src/main/java/com/seyijs/springbootmongodb/SpringbootMongodbApplication.java
@@ -3,12 +3,20 @@ package com.seyijs.springbootmongodb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvException;
 
 @SpringBootApplication
 @EnableSwagger2
 public class SpringbootMongodbApplication {
 
 	public static void main(String[] args) {
+
+
+		Dotenv dotenv = Dotenv.configure().directory("./java-mongodb-restapi").systemProperties().load();
+
+		System.out.println(dotenv.get("MONGODB_URI"));
+
 		SpringApplication.run(SpringbootMongodbApplication.class, args);
 	}
 
